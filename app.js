1f72b83cf0867b529c0828919f8b6d24f980e6e0
Uthman Yakub <yakubau1000@gmail.com>
2022-05-19 16:09:47 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+let nav = document.querySelector("#nav");
+// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+// const mainbottom = document.querySelector("#main").offset().top();
+
+// on scroll,
+window.addEventListener("scroll", function () {
+	// we round here to reduce a little workload
+	// stop = Math.round(window.scrollTop());
+	if (window.pageYOffset > 80) {
+		nav.classList.add("past-main");
+		nav.classList.add("transution-effect");
+		// nav.classList.add("fixed-top");
+	} else {
+		nav.classList.remove("past-main");
+		// nav.classList.remove("fixed-top");
+	}
+});
+
 const hamburger = document.querySelector(".navbar-toggler");
 const navmenu = document.querySelector("#navbarSupportedContent");
 
 hamburger.addEventListener("click", () => {
 	navmenu.classList.toggle("show");
-	navmenu.classList.toggle("nav--active");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
 	n.addEventListener("click", () => {
 		navmenu.classList.remove("show");
-		navmenu.classList.toggle("nav--active");
 	})
 );
 
